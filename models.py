f151df386c3cd5a5419b1363afd2f02231ee63c7
shAd0re <risha1r@cmich.edu>
2024-11-03 11:44:18 -0500

    Cleaned

--- models.py
+++ models.py
@@ -1,35 +1,46 @@
 from database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 class Appointment(Base):
     __tablename__ = 'appointments'
-    apnt_id: int =  Column(Integer, primary_key=True, index=True)
-    doctor_id: int =  Column(Integer)
-    patient_id: int =  Column(Integer)
-    appointment_date: str = Column(String)
-    notes: str = Column(String)
-    prescrition: str = Column(String)
+    apnt_id =  Column(Integer, primary_key=True, index=True)
+    doctor_id =  Column(Integer, ForeignKey("doctors.doctor_id"))
+    patient_id =  Column(Integer, ForeignKey("patients.patient_id"))
+    appointment_date = Column(String)
+    notes = Column(String)
+    prescrition = Column(String)
     
 
 class Patient(Base):
     __tablename__ = 'patients'
-    patient_id: int =  Column(Integer, primary_key=True, index=True)
-    user_id: str = Column(String)
-    user_name: str = Column(String)
-    first_name: str = Column(String)
-    last_name: str = Column(String)
-    DOB: str = Column(String)
-    gender: str = Column(String)
-    phone: str = Column(String)
+    patient_id =  Column(Integer, primary_key=True, index=True)
+    user_name = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    DOB = Column(String)
+    gender = Column(String)
+    phone = Column(String)
+    hashed_password = Column(String)
 
 class Doctor(Base):
     __tablename__ = 'doctors'
-    doctor_id: int =  Column(Integer, primary_key=True, index=True)
-    user_id: str = Column(String)
-    user_name: str = Column(String)
-    first_name: str = Column(String)
-    last_name: str = Column(String)
-    DOB: str = Column(String)
-    specialization: str = Column(String)
-    phone: str = Column(String)
+    doctor_id =  Column(Integer, primary_key=True, index=True)
+    user_name = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    DOB = Column(String)
+    specialization = Column(String)
+    phone = Column(String)
+    hashed_password = Column(String)
 
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String)
+    user_name = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    DOB = Column(String)
+    gender = Column(String)
+    phone = Column(String)
