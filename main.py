0c656edb3ce8d89374031ac6b3ecb85989d07c37
shAd0re <risha1r@cmich.edu>
2024-11-13 23:24:33 -0500

    templates added

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 #Body is used for http requests except get
 import models
 from database import engine
 from routers import auth, appointments, billing
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+templates = Jinja2Templates(directory="templates")
+
 
 app = FastAPI()
 
@@ -12,5 +18,11 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(appointments.router)
 app.include_router(billing.router)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def render_login(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 
